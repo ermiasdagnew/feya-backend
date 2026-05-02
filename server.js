95d132ec989ba1e09e43a7fc9383b98282993235
Ermias Aweke <ermiasdagnewa@gmail.com>
2026-05-02 14:55:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+const express = require("express");
+const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("FEYA backend is running 🚀");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
